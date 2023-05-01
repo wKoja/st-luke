34e78f195686a18f155074a151289ed49662805c
wKoja <cfwilliam.ferreira@gmail.com>
2023-05-01 15:20:57 -0300

    custom stuff

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "DejaVu Sans Mono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = { "Sarasa Mono SC Nerd:pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
