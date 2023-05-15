7a528847ce08dea6012e1284e20ea223a81d81e7
wKoja <cfwilliam.ferreira@gmail.com>
2023-05-15 15:19:40 -0300

    jetbrains nerd font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono Nerd Font:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "Sarasa Mono SC Nerd:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "JetBrainsMono Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font2[] = { "JetBrainsMono Nerd Font:pixelsize=17:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
